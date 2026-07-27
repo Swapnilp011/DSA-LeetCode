196b84f494b9bcaea493c23c959ece1040d15eec
Swapnil Pawar <160909748+Swapnilp011@users.noreply.github.com>
2026-07-27 10:05:48 +0530

    Time: 2 ms (2.74%), Space: 45.7 MB (99.05%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -4,12 +4,12 @@ class Solution {
      int n=mat.length;
 
      for (int i=0; i<n; i++){
-        for ( int j=0; j<n; j++){
-            if(i==j || i+j==n-1){
-                sum+= mat[i][j];
+        for(int j=0; j<n; j++){
+            if( i==j || i+j==n-1){
+                sum = sum+ mat[i] [j];
             }
         }
-     }   
+     }
      return sum;
     }
 }
